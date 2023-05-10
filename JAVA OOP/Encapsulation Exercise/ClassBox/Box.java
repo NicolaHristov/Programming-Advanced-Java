2c9cf47631e5b327ac915e7dac80298fe2741833
NicolaHristov <124876291+NicolaHristov@users.noreply.github.com>
2023-05-10 10:23:55 +0300

    ClassBox

---
+++ JAVA OOP/Encapsulation Exercise/ClassBox/Box.java
@@ -0,0 +1,55 @@
+package ClassBox;
+
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        setLength(length);
+        setWidth(width);
+        setHeight(height);
+    }
+    //+ 	Box (double length, double width, double height)
+    //-	setLength(double): void
+    //-	setWidth(double): void
+    //-	setHeight(double): void
+    //+	calculateSurfaceArea (): double
+    //+	calculateLateralSurfaceArea (): double
+    //+	calculateVolume (): double
+
+
+    private void setLength(double length) {
+        if(length<=0){
+            throw new IllegalArgumentException("Length cannot be zero or negative.");
+        }
+        this.length = length;
+    }
+
+    private void setWidth(double width) {
+        if(width<=0){
+            throw new IllegalArgumentException("Width cannot be zero or negative.");
+        }
+        this.width = width;
+    }
+
+    private void setHeight(double height) {
+        if(height<=0){
+            throw new IllegalArgumentException("Height cannot be zero or negative.");
+        }
+        this.height = height;
+    }
+    //Volume = lwh
+    //Lateral Surface Area = 2lh + 2wh
+    //Surface Area = 2lw + 2lh + 2wh
+
+    public double calculateSurfaceArea (){
+      return  2 * length * width + 2 * length* height + 2 * width * height;
+    }
+    public double calculateLateralSurfaceArea(){
+             return 2 * length * height + 2 * width * height;
+    }
+    public double calculateVolume (){
+        return length*height*width;
+    }
+}
